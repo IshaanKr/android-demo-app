e2ebe7be354158a8df3b40c95fcfa54b14f258f1
Ishaan Kumar <i.k.ishaan786@gmail.com>
2021-04-29 06:33:54 +0530

    minor

--- app/src/main/java/com/xymenapps/demo/main/adapters/MainRecyclerViewHolder.kt
+++ app/src/main/java/com/xymenapps/demo/main/adapters/MainRecyclerViewHolder.kt
@@ -7,7 +7,8 @@ import com.xymenapps.demo.R
 import com.xymenapps.demo.main.models.MainRecyclerViewModel
 
 class MainRecyclerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    val title: TextView = itemView.findViewById(R.id.main_recyclerview_item_title)
+    private val title: TextView = itemView.findViewById(R.id.main_recyclerview_item_title)
+
     fun bindData(item: MainRecyclerViewModel) {
         title.text = item.title
     }
